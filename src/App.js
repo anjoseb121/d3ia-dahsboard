d039e7461dce4b8e8c8705a87cf9d61524e7503a
Antonio <antonio@nativapps.mobi>
2017-08-29 17:52:30 -0500

    Add StreamGraph component

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ appData.forEach((d, i) => {
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.onResize = this.onResize.bind(this);
+        this.state = { screenWidth: 1000, screenHeight: 500 }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize, false);
+        this.onResize();
+    }
+
+    onResize() {
+        this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight - 70})
+    }
+
+
     render() {
 
         const colorScale = scaleThreshold().domain([5, 10, 20, 30, 50]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"]);
@@ -29,9 +45,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Dashboard</h2>
         </div>
-          <StreamGraph colorScale={colorScale} data={appData} size={[1000, 250]} />
-          <WorldMap colorScale={colorScale} data={appData} size={[500, 400]} />
-          <BarChart colorScale={colorScale} data={appData}  size={[500, 400]} />
+          <StreamGraph colorScale={colorScale} data={appData} size={[this.state.screenWidth, this.state.screenHeight / 2]} />
+          <WorldMap colorScale={colorScale} data={appData} size={[this.state.screenWidth / 3, this.state.screenHeight / 2]} />
+          <BarChart colorScale={colorScale} data={appData}  size={[this.state.screenWidth / 3, this.state.screenHeight / 2]} />
       </div>
     );
   }
